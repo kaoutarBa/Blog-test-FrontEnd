efc8c4d99e9b9993ede46448733488752a7a9888
kaoutarBa <baldi.kaoutar.ma@gmail.com>
2020-10-01 19:14:41 +0100

    filter items

--- src/components/MyBlog/MyBlog.js
+++ src/components/MyBlog/MyBlog.js
@@ -5,10 +5,12 @@ import SearchField from "components/SearchField";
 import Card from "components/Card";
 
 
+
 const MyBlog=()=>{
+    const [filteredItems,setFilteredItems]=useState([]);
     const [items,setItems]=useState([]);
-    const [searchItems,setSearchItems]=useState([]);
-    const [inputValue,setInputValue]=useState('');
+    const [inputValue,setInputValue]=useState("");
+    console.log(filteredItems);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -23,24 +25,45 @@ const MyBlog=()=>{
                 return itemsSet;
         });
     };
+    
         fetchData();
+    
     }, []);
 
     const handleChange=(value)=>{
+        const newItems = items
+            .map(item => item.title)
+            .filter(title => {
+            const searchExpression = new RegExp(value, "gi");
+            return title.match(searchExpression);
+            });
+        if (value !== "") {
+        setFilteredItems([
+            ...items.filter(item => newItems.includes(item.title))
+        ]);
         setInputValue(value);
-    };
-    console.log(items);
+        } else {
+            setFilteredItems([]);
+            setInputValue(value);
+        } };
     return(
         <Container>
             <h1 > My Blog</h1>
             <SearchField  value={inputValue} handleChange={handleChange}/>
             <CardGrid>
-                {items.map((item,index)=>
+                {filteredItems[0]?(filteredItems.map((item,index)=>
                 <Card key={index} 
                 gridArea={index}
                 title={item.title}
                 body={item.body} 
-                itemId={item.id}/>)}
+                itemId={item.id}/>)):
+                (items.map((item,index)=>
+                <Card key={index} 
+                gridArea={index}
+                title={item.title}
+                body={item.body} 
+                itemId={item.id}/>))
+                }
             </CardGrid>
         </Container>
     )
