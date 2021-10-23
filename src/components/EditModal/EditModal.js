ff903bc84b9d9a7ff303749435ad2de5e50b178d
kaoutarBa <baldi.kaoutar.ma@gmail.com>
2021-10-23 13:59:29 +0100

    modif

--- src/components/EditModal/EditModal.js
+++ src/components/EditModal/EditModal.js
@@ -29,7 +29,6 @@ const EditModal=({
         });        
         setItems([...newItems]);
         setDisplayModal(false);
-       
     };
     const handleCancel=()=>{
         setDisplayModal(false);
